e7dd150f21d59d2ad4f1f6fd2a076efa4f8e9b45
Adebayo Opesanya <bayoopesanya@gmail.com>
2021-01-17 05:04:03 +0100

    feat: setup secrets for config

---
+++ src/config/secrets.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+const logger = require('../util/logger');
+
+dotenv.config({ path: '.env' });
+
+const { APP_PORT, MONGODB_URI } = process.env;
+const ENVIRONMENT = process.env.NODE_ENV || 'development';
+
+if (!MONGODB_URI) {
+  logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
+  process.exit(1);
+}
+
+if (!APP_PORT) {
+  logger.error('No app port. Set APP_PORT environment variable.');
+  process.exit(1);
+}
+
+module.exports = { APP_PORT, MONGODB_URI, ENVIRONMENT };
